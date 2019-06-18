be145838eec2df440ca13d2c1b265babd24baec4
student <tom.doesburg@gmail.com>
2019-06-18 14:32:22 +1200

    idk

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,66 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h3>Bridey's</h3>
+          <h1>Mario Kart Tournament</h1>
+        </div>
+      </div>
+
+      <div className="name-form-container">
+        <form className="nameForm" action="yeet">
+          <ul>
+            <li>
+              <label>Player 1</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+            <li>
+              <label>Player 2</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+            <li>
+              <label>Player 3</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+            <li>
+              <label>Player 4</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+            <li>
+              <label>Player 5</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+            <li>
+              <label>Player 6</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+            <li>
+              <label>Player 7</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+            <li>
+              <label>Player 8</label>
+              <input type="text" name="name" maxLength="100" />
+              <span>What's your name?</span>
+            </li>
+          </ul>
+          <br />
+          <div className="button-div">
+            <input type="submit" value="Race!" />
+          </div>
+        </form>
+      </div>
+    </div>
+
   )
 }
 
