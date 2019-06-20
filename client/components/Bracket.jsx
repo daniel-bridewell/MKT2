380fe062916f536821a49976888580134f36dbde
Daniel Bridewell <48977884+daniel-bridewell@users.noreply.github.com>
2019-06-20 15:36:44 +1200

    sdfgsdg

--- client/components/Bracket.jsx
+++ client/components/Bracket.jsx
@@ -3,31 +3,37 @@ import React from 'react'
 const Bracket = () => {
 
   return (
+  <div>
+    <br/>
+    <br/>
     <section id="bracket">
       <div className="container">
         <div className="split split-one">
           <div className="round round-one current">
-            <div className="round-details">Round 1<br /></div>
-            <ul className="matchup">
-              <li className="player player-top">Big Dan</li>
-              <li className="player player-bottom">Tess</li>
-            </ul>
-            <ul className="matchup">
-              <li className="player player-top">Tom</li>
-              <li className="player player-bottom">Pineapple Dan</li>
-            </ul>
-            <ul className="matchup">
-              <li className="player player-top">Bridey</li>
-              <li className="player player-bottom">Pano</li>
-            </ul>
-            <ul className="matchup">
-              <li className="player player-top">Dan VA</li>
-              <li className="player player-bottom">Hanh</li>
-            </ul>
+            <div className="round1-wrapper">
+              <div className="round-names">Round 1<br /></div>
+              <ul className="matchup">
+                <li className="player player-top">Big Dan</li>
+                <li className="player player-bottom">Tess</li>
+              </ul>
+              <ul className="matchup">
+                <li className="player player-top">Tom</li>
+                <li className="player player-bottom">Pineapple Dan</li>
+              </ul>
+              <ul className="matchup">
+                <li className="player player-top">Bridey</li>
+                <li className="player player-bottom">Pano</li>
+              </ul>
+              <ul className="matchup">
+                <li className="player player-top">Dan VA</li>
+                <li className="player player-bottom">Q</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </section>
+  </div>
   )
 }
 
