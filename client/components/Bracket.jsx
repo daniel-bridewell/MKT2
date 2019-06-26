991883cfc54d93217075ed8fc58195737fb3ad8d
Daniel Bridewell <bridey@Daniels-MacBook-Pro.local>
2019-06-27 11:13:50 +1200

    bracket

--- client/components/Bracket.jsx
+++ client/components/Bracket.jsx
@@ -3,38 +3,57 @@ import React from 'react'
 const Bracket = () => {
 
   return (
-  <div>
+  <div className="center-all">
     <br/>
     <br/>
     <section id="bracket">
-      <div className="container">
-        <div className="split split-one">
-          <div className="round round-one current">
-            <div className="round1-wrapper">
-              <div className="round-names">Round 1<br /></div>
-              <ul className="matchup">
-                <li className="player player-top">Big Dan</li>
-                <li className="player player-bottom">Tess</li>
-              </ul>
-              <ul className="matchup">
-                <li className="player player-top">Tom</li>
-                <li className="player player-bottom">Pineapple Dan</li>
-              </ul>
-              <ul className="matchup">
-                <li className="player player-top">Bridey</li>
-                <li className="player player-bottom">Pano</li>
-              </ul>
-              <ul className="matchup">
-                <li className="player player-top">Dan VA</li>
-                <li className="player player-bottom">Q</li>
-              </ul>
-            </div>
-          </div>
+    <div className="container">
+    <div className="split split-one">
+        <div className="round round-one current">
+            <div className="round-details">Round 1<br/><span className="date">Welcome!</span></div>
+            <ul className="matchup">
+                <li className="team team-top">placeholder</li>
+                <li className="team team-bottom">placeholder</li>
+            </ul>
+            <ul className="matchup">
+                <li className="team team-top">placeholder</li>
+                <li className="team team-bottom">placeholder</li>
+            </ul>
+            <ul className="matchup">
+                <li className="team team-top">placeholder</li>
+                <li className="team team-bottom">placeholder</li>
+            </ul>
+            <ul className="matchup">
+                <li className="team team-top">placeholder</li>
+                <li className="team team-bottom">placeholder</li>
+            </ul>                                               
         </div>
-      </div>
-    </section>
+
+        <div className="round round-two current">
+            <div className="round-details">Semi-Finals<br/><span className="date">Don't choke</span></div>         
+            <ul className="matchup">
+                <li className="team team-top">&nbsp;<span className="score">&nbsp;</span></li>
+                <li className="team team-bottom">&nbsp;<span className="score">&nbsp;</span></li>
+            </ul>   
+            <ul className="matchup">
+                <li className="team team-top">&nbsp;<span className="score">&nbsp;</span></li>
+                <li className="team team-bottom">&nbsp;<span className="score">&nbsp;</span></li>
+            </ul>                                     
+        </div>
+        
+        <div className="round round-three current">
+            <div className="round-details">Finals<br/><span className="date">Congrats!</span></div>         
+            <ul className="matchup">
+                <li className="team team-top">&nbsp;<span className="score">&nbsp;</span></li>
+                <li className="team team-bottom">&nbsp;<span className="score">&nbsp;</span></li>
+            </ul>                                       
+        </div>    
+    </div>
+  </div>
+  </section>
   </div>
   )
 }
+
 
 export default Bracket
